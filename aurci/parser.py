d0bb46ff8c2a8a87c0cdda3baed0e81ef8c5de01
bionade24 <o.roesler@oscloud.info>
2019-09-29 21:19:32 +0200

    Added package statement

--- aurci/parser.py
+++ aurci/parser.py
@@ -14,6 +14,7 @@ def main(argv):
     parser=argparse.ArgumentParser(prog='dockerctl', add_help=True)
     
     parser.add_argument('command', choices=['clone', 'build'])
+    parser.add_argument('package', type=str)
 
     args = parser.parse_args(argv)
     commands(args.command, args.package)
