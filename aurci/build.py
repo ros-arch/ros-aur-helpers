28b9e22ee59aecb2da46965666d5c25821e14865
bionade24 <o.roesler@oscloud.info>
2019-10-05 20:13:11 +0200

    Moved functions in build.py into a class

--- aurci/build.py
+++ aurci/build.py
@@ -1,22 +1,29 @@
 import subprocess
 import os
 
+class Build:
+    def __init__(self, package):
+        self.package = package
+        self.path = "./packages/{0}/".format(package)
 
-def dmakepkg(package):
-    path = "./packages/{0}/".format(package)
-    if os.path.isfile(path + "PKGBUILD"):
-        try:
-            subprocess.run(["dmakepkg", "-xy"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, cwd=path, check=True)
-        except subprocess.CalledProcessError:
-            with open("failed.txt", "w") as fobj:
-                fobj.write(package + "\n")
-                pass
-    
-def build(package):
-    if os.path.exists("failed.txt"):
-        os.remove("failed.txt")
-    if package=="all":
-        for folder in os.listdir("./packages"):
-            dmakepkg(folder)    
-    else:
-        dmakepkg(package)
+
+    def dmakepkg(self):
+        if os.path.isfile(self.path + "PKGBUILD"):
+            try:
+                subprocess.run(["dmakepkg", "-xy"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, cwd=self.path, check=True)
+                return 0
+            except subprocess.CalledProcessError:
+                with open("failed.txt", "w") as fobj:
+                    fobj.write(self.package + "\n")
+                    return 1
+        else:
+            raise BaseException("No PKBUILD existing: ", self.path)
+
+    def build(self):
+        if os.path.exists("failed.txt"):
+            os.remove("failed.txt")
+        if self.package=="all":
+            for folder in os.listdir("./packages"):
+                Build(folder).dmakepkg
+        else:
+            Build(self.package).dmakepkg()
