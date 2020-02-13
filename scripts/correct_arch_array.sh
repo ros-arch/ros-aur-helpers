3fbd77368b8edc6798be7886ea7d3f12fa185cf8
bionade24 <o.roesler@oscloud.info>
2020-02-13 18:05:22 +0100

    Add scripts/correct_arch_array.sh

---
+++ scripts/correct_arch_array.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+for i in `ls packages`;
+do
+	if [pacman -Si $i | egrep -q 'cpp|boost']; then
+		echo "$i contains c++ code"
+		cd packages/$i
+		sed -i "s/arch=('any')/arch=('i686' 'x86_64' 'aarch64' 'armv7h' 'armv6h')/g" PKGBUILD
+		IFS="="
+		cat PKGBUILD | grep "pkgrel" | read -r name value
+		pkgrel = $value + 1
+		sed -i "s/pkgrel=$value/pkgrel=$pkgrel/g" PKGBUILD
+		makepkg --printsrcinfo > .SRCINFO
+		git add PKGBUILD .SRCINFO
+		git commit -m "Fixed arch=() array"
+		git push origin master;
+	else
+		echo "$i does NOT contain c++ code";
+	fi
