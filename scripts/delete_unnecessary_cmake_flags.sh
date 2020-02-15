b838131d6c0fe3e868d6ba73041fd17e61e21765
bionade24 <o.roesler@oscloud.info>
2020-02-15 12:52:08 +0100

    fix delete_unnecessary_cmake_flags.sh and add another cmake flag

--- scripts/delete_unnecessary_cmake_flags.sh
+++ scripts/delete_unnecessary_cmake_flags.sh
@@ -3,3 +3,5 @@
 for i in `ls packages`; do
 	sed -i '/-DPYTHON_BASENAME/d' packages/$i/PKGBUILD
 	sed -i '/-DPYTHON_LIBRARY/d' packages/$i/PKGBUILD
+	sed -i '/-DPYTHON_INCLUDE_DIR/d' packages/$i/PKGBUILD
+done
