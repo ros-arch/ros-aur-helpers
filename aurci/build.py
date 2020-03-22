21405fab8afeee2df7567fd93b445f33ad052fa3
bionade24 <o.roesler@oscloud.info>
2020-03-22 22:11:36 +0100

    Delete old package version in repository before adding new ones

--- aurci/build.py
+++ aurci/build.py
@@ -42,6 +42,10 @@ class Packages:
         else:
             self.dmakepkg()
 
+    def del_old_pkg(self):
+        for pkg in glob.iglob(".repository/*{0}*.pkg.tar.*".format(self.package)):
+            os.remove(pkg)
+
     def mvpkg(self):
         for pkg_path in glob.iglob(self.path + "/*pkg.tar*"):
             shutil.move(pkg_path, "./repository/")
@@ -75,6 +79,7 @@ class Packages:
         else:
             try:
                 self.dmakepkg()
+                self.del_old_pkg()
                 self.mvpkg()
                 self.aur_push()
             except RuntimeWarning:
