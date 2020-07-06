1e89ed0df324941e8b38322fe67a16aeb1c2546f
bionade24 <o.roesler@oscloud.info>
2020-07-06 14:24:06 +0200

    Add query_yes_no()

--- aurci/general.py
+++ aurci/general.py
@@ -66,3 +66,33 @@ class Routines:
         config = configparser.ConfigParser()
         config.read('config.ini')
         return config['CI']['GH_ORGANIZATION'].rstrip("arch")
+
+    @staticmethod
+    def query_yes_no(question, default="yes"):
+        """
+        Ask a yes/no question via input() and return their answer.
+        "question" is a string that is presented to the user.
+        "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+        The "answer" return value is one of "yes" or "no".
+        """
+        valid = {"yes": "yes", "y": "yes", "ye": "yes",
+                 "no": "no", "n": "no"}
+        if not default:
+            prompt = " [y/n] "
+        elif default == "yes":
+            prompt = " [Y/n] "
+        elif default == "no":
+            prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
+        while True:
+            print(question + prompt)
+            choice = input().lower()
+            if default is not None and choice == '':
+                return default
+            elif choice in valid.keys():
+                return valid[choice]
+            else:
+                print("Please respond with 'yes' or 'no' (or 'y' or 'n').")
