fd686166cf14569e8ea54df68b42f9afc72dcbaa
bionade24 <o.roesler@oscloud.info>
2020-07-06 16:28:30 +0200

    Rewrite scripts/delete_unnecessary_cmake_flags.sh for having same the features of the other scripts

--- scripts/delete_unnecessary_cmake_flags.sh
+++ scripts/delete_unnecessary_cmake_flags.sh
@@ -1,7 +1,22 @@
-#/bin/bash
+#!/bin/zsh
 
-for i in `ls packages`; do
-	sed -i '/-DPYTHON_BASENAME/d' packages/$i/PKGBUILD
-	sed -i '/-DPYTHON_LIBRARY/d' packages/$i/PKGBUILD
-	sed -i '/-DPYTHON_INCLUDE_DIR/d' packages/$i/PKGBUILD
+for i in `ls packages`;
+do
+    if grep 'DPYTHON_LIBRARY' packages/$i/PKGBUILD; then
+        echo "$i has unnecessary cmake args"
+        cd packages/$i
+        sed -i '/-DPYTHON_BASENAME/d' PKGBUILD
+        sed -i '/-DPYTHON_LIBRARY/d' PKGBUILD
+        sed -i '/-DPYTHON_INCLUDE_DIR/d' PKGBUILD
+        grep "pkgrel" PKGBUILD | IFS="=" read -r name value
+        (( pkgrel = $value + 1 ))
+        sed -i "s/pkgrel=$value/pkgrel=$pkgrel/g" PKGBUILD
+        makepkg --printsrcinfo > .SRCINFO
+        git add PKGBUILD .SRCINFO
+        git commit -m "Delete unnecessary cmake args"
+        git push origin master
+        cd -;
+    else
+        echo "$i doesn't have unnecessary cmake args";
+    fi
 done
